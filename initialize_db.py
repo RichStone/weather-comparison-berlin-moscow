f4a9b8183b6d8f1af1e556ec24d3963e2af5fb42
Rich Stone <richard.had@hotmail.de>
2017-09-10 18:00:30 +0200

    add database initialization file

---
+++ initialize_db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base, Weather, Category, City
+
+engine = create_engine('sqlite:///weather.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+berlin = City(id=10382, name='Berlin')
+moscow = City(id=27612, name='Moscow')
+
+session.add(berlin)
+session.add(moscow)
+
+session.commit()
